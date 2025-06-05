efe1f31f634516b4defde25b4e94fcb389a4138c
Cazelex <134857108+Cazelex@users.noreply.github.com>
2025-06-05 18:23:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
- function changeText() {
-            document.getElementById("myParagraph").textContent = "The text has been changed!";
-        }
-
+function changeText() {
+     document.getElementById("myParagraph").textContent = "The text has been changed!";
+}
 function downloadFile() {
-    const content = "Hello, world!"; // Your file content
-    const blob = new Blob([content], { type: "text/plain" });
     const link = document.createElement("a");
-
-    link.href = URL.createObjectURL(blob);
-    link.download = "example.txt"; // File name
+    link.href = "download.txt";
+    link.download = "download.txt";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
